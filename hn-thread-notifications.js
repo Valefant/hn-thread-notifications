6e034298e48a4f0feeedad631bd9377a1f0bd0ac
Valentino <valentino.bernardo_ciddio@smail.th-koeln.de>
2020-05-04 19:06:31 +0200

    Cleanup the script

--- hn-thread-notifications.js
+++ hn-thread-notifications.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hn-thread-notifications
 // @namespace    hackernews
-// @version      0.1
+// @version      0.2
 // @description  Indicates visually if messgages are read or unread within any thread on hackernews.
 //               A message is considered read when the thread you are on is refreshed.
 // @author       Valefant
@@ -14,28 +14,32 @@
     'use strict';
 
     const threadId = document.URL.split("=")[1];
-    const allIds = [document.querySelector(".athing"), ...document.querySelectorAll("tr .athing.comtr")];
-    const allIdsString = allIds.map(el => el.id).join(" ");
+    const allMessageIds = [document.querySelector(".athing"), ...document.querySelectorAll("tr .athing.comtr")];
+    const allMessageIdsAsString = allMessageIds.map(el => el.id).join(" ");
     const retreivedIds = GM_getValue(threadId);
     const unread = createIndicator("green");
     const read = createIndicator("red");
 
     if (GM_getValue(threadId) === undefined) {
-        GM_setValue(threadId, allIdsString);
-        [...document.querySelectorAll(".comhead")].forEach(commentHead => commentHead.prepend(createIndicator("green").cloneNode(true)))
+        // GM_setValue does not support lists therefore I have to encode the ids as one big string
+        GM_setValue(threadId, allMessageIdsAsString);
+        // Mark messages as unread
+        [...document.querySelectorAll(".comhead")].forEach(commentHead => commentHead.prepend(createIndicator("green").cloneNode(true)));
     } else {
         retreivedIds
         .split(" ")
+        .filter(id => document.querySelector(`[id='${id}']`) !== null)
         .map(id => document.querySelector(`[id='${id}']`).querySelector(".comHead"))
         .forEach(head => head.prepend(read.cloneNode(true)));
 
-        const newIds = allIdsString.split(" ").filter(id => !retreivedIds.split(" ").includes(id));
-        console.log(newIds)
-        newIds
+        // Mark messages as read by comparing the ids retreived from the storage minus the ids which are determined on entering the thread
+        allMessageIdsAsString
+        .split(" ")
+        .filter(id => !retreivedIds.split(" ").includes(id))
         .map(id => document.querySelector(`[id='${id}']`).querySelector(".comHead"))
         .forEach(head => head.prepend(unread.cloneNode(true)));
 
-        GM_setValue(threadId, allIdsString);
+        GM_setValue(threadId, allMessageIdsAsString);
     }
 })();
 
